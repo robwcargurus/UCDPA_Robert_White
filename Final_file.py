f48bffb9ca78d77e61b470e9c708d005a3a9ae2c
robwcargurus <rwhite@cargurus.com>
2021-09-16 16:26:31 +0100

    Update + Final

--- Final_file.py
+++ Final_file.py
@@ -159,6 +159,11 @@ print(volume_cars.describe())
 cars = [premium_cars, volume_cars]
 total_cars = pd.concat(cars)
 
+sns.lineplot(total_cars['year'], total_cars['price'],
+             hue=total_cars['brandType']).set_title('MPG of Premium Cars vs Volume Cars')
+plt.savefig("premvsvol_price.png")
+plt.show()
+
 sns.lineplot(total_cars['year'], total_cars['mpg'],
              hue=total_cars['brandType']).set_title('MPG of Premium Cars vs Volume Cars')
 plt.savefig("premvsvol_mpg.png")
